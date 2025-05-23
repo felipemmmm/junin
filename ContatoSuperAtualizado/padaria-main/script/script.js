bad0c67b69350d14a088380ce8576460a4d7b59a
Felipe Martins <felipemrtns6002199@gmail.com>
2025-05-23 09:56:05 -0300

    Add files via upload

---
+++ ContatoSuperAtualizado/padaria-main/script/script.js
@@ -0,0 +1,10 @@
+function send_message() {
+    let name = document.getElementById('name').value; 
+    let sobrenome =  document.getElementById('surname').value;
+    let email =  document.getElementById('email').value;
+    let mensagem =  document.getElementById('message').value;
+    let teste = document.getElementById('teste');
+
+    alert("Mensagem Enviada!")
+
+}
